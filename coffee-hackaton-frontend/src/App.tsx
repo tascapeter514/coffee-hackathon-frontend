ecb310592ab8949920f27b2c1635efef5747762c
petertasca <tascapeter@gmail.com>
2025-05-18 15:59:34 -0400

    feat: exercise dropdown

--- coffee-hackaton-frontend/src/App.tsx
+++ coffee-hackaton-frontend/src/App.tsx
@@ -3,11 +3,16 @@ import sampleExercises from './fitnessData'
 
 import './App.css'
 
+
+const movementPatterns = ['upper pull', 'upper push', 'lower pull', 'lower push']
+
+
+
 function App() {
   
 
   return (
-    <>
+    <main className='container'>
       <h1>Welcome to Fight Bytes</h1>
 
        {/* Dropdown for MovementPattern */}
@@ -16,13 +21,27 @@ function App() {
         <label className='movement-pattern-dropdown'>
           Select your movement pattern
           <select>
-            <option></option>
+            {movementPatterns.map (pattern => {
+              return(
+                <option>{pattern}</option>
+              )
+            })}
+          </select>
+        </label>
+        <label className='movement-pattern-dropdown'>
+          Select your exercise
+          <select>
+            {sampleExercises.map (exercise => {
+              return(
+                <option>{exercise.name}</option>
+              )
+            })}
           </select>
         </label>
         <button type='submit'>Submit</button>
-
       </form>
        {/* Dropdown for Exercise Selection */}
+
        {/* Input for weight */}
        
 
@@ -31,7 +50,7 @@ function App() {
       
 
       
-    </>
+    </main>
   )
 }
 
